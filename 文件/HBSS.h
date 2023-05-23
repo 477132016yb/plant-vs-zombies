16e44411503de2f62cd21150e7f0e8027ea698cc
477132016yb <101654509+477132016yb@users.noreply.github.com>
2023-05-23 19:05:57 +0800

    Update HBSS.h

--- 文件/HBSS.h
+++ 文件/HBSS.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "plant.h"
-//º®±ùÉäÊÖ
+//ÂºÂ®Â±Ã¹Ã‰Ã¤ÃŠÃ–
 class HBSS :
     public plant
 {
 public:
     HBSS();
+    int count;
     virtual void jineng(vector<bullet>& bullets, vector<zm*>& zms, vector<sunshine>& ball);
 };
 
